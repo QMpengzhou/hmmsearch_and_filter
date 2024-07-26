d1e96e18ce314733f245406ff2d3efc4a5b6145e
Peng Zhou <p.zhou@se23.qmul.ac.uk>
2024-07-26 18:17:34 +0100

    add one argument "--merged" to allow merge every fasta file into single one.

--- extraction_script.sh
+++ extraction_script.sh
@@ -2,13 +2,17 @@
 
 evalue=$1
 input_folder=$2
-hmm_file=$(basename "$3")
+merged_fasta=$3
+hmm_file=$(basename "$4")
 hmm_name="${hmm_file%.hmm}"
-seq_db=$4
+seq_db=$5
 
 for file in "$input_folder"/*.domtblout; do
     file_domtblout=$(basename "$file")
     file_name="${file_domtblout%."$hmm_name".domtblout}"
     cat "$input_folder"/"$file_domtblout" | grep -v "#" | tr -s " " "\t" | awk -v evalue="$evalue" '$7 < evalue && $12 < evalue' | cut -f 1 | sort -u > "${input_folder}/${file_name}.${hmm_name}.ids"
     seqkit grep -f "${input_folder}/${file_name}.${hmm_name}.ids" "${seq_db}/${file_name}.fasta" > "${input_folder}/${file_name}.${hmm_name}.fasta"
-done 
+done
+if [ "$merged_fasta" = "true" ]; then
+  seqkit concat --full "${input_folder}"/*."${hmm_name}".fasta > "merged_${hmm_name}.fasta"
+fi
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import subprocess
 import os
 
 
-def extract_pipe(ev, op_pf, hf, sdb):
-    arguments = [str(ev), op_pf, hf, sdb]
+def extract_pipe(ev, op_pf, me, hf, sdb):
+    merged_fasta = "true" if me else "false"
+    arguments = [str(ev), op_pf, merged_fasta, hf, sdb]
     extraction_script_path = "./extraction_script.sh"
     command = [extraction_script_path] + arguments
     result = subprocess.run(command, capture_output=True, text=True)
@@ -14,7 +15,7 @@ def extract_pipe(ev, op_pf, hf, sdb):
         print("program failed with return code:", result.returncode)
 
 
-def search_pipe(ev, op, hf, sdb):  # run script with hmmsearch
+def search_pipe(ev, op, me, hf, sdb):  # run script with hmmsearch
     if op is not None:
         output_prefix = op
     else:
@@ -25,7 +26,7 @@ def search_pipe(ev, op, hf, sdb):  # run script with hmmsearch
     command = [hmmsearch_script_path] + arguments
     result = subprocess.run(command, capture_output=True, text=True)
     if result.returncode == 0:
-        extract_pipe(ev, output_prefix, hf, sdb)
+        extract_pipe(ev, output_prefix, me, hf, sdb)
     else:
         print("program failed with return code:", result.returncode)
 
@@ -39,7 +40,10 @@ parser_f = subparsers.add_parser("search", help="perform hmmsearch and filter th
 parser_f.add_argument("-e", "--evalue", metavar="", dest="evalue", type=float, default=0.0001,
                       help="E-value threshold to filter hits, default is 0.0001")
 parser_f.add_argument("-o", "--output_prefix", type=str, metavar="", dest="output_prefix",
-                      help="Prefix in output directory, default is hmmfile prefix + hmmer_search")
+                      help="Prefix in output directory, default is hmmfile prefix + \"hmmer_search\"")
+parser_f.add_argument("-m", "--merged", action="store_true",
+                      help="An optional argument to provide a merged fasta file for further multiple sequence "
+                           "alignment.")
 parser_f.add_argument("hmmfile", metavar="hmmfile", type=str, help="the query HMM profile")
 parser_f.add_argument("seqdb", metavar="seqdb", type=str,
                       help="the target directory with sequences in fasta format")
@@ -47,4 +51,4 @@ parser_f.add_argument("seqdb", metavar="seqdb", type=str,
 args = parser.parse_args()
 
 if args.command == "search":
-    search_pipe(args.evalue, args.output_prefix, args.hmmfile, args.seqdb)
+    search_pipe(args.evalue, args.output_prefix, args.merged, args.hmmfile, args.seqdb)
